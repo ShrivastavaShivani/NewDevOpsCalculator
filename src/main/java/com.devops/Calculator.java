f2cb1194ef0fe4f39e2e6b064db469f531b6430e
chikushrivastava <shivanishrivastava04@gmail.com>
2020-05-11 22:42:41 +0530

    Added Subtraction

--- src/main/java/com.devops/Calculator.java
+++ src/main/java/com.devops/Calculator.java
@@ -5,8 +5,14 @@ import java.io.*;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("add 6+4="+add(6,4));
+        System.out.println("6 - 3 = " + subtract(6,3));
     }
     public static int add(int a,int b){
         return a + b;
     }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
 }
--- src/test/java/com.devops/CalculatorTest.java
+++ src/test/java/com.devops/CalculatorTest.java
@@ -22,4 +22,13 @@ public class CalculatorTest {
         int result = calculator.add(a, b);
         Assert.assertEquals(expectedResult, result);
     }
+
+    @Test
+    public void testSubtract() {
+        int a = 25;
+        int b = 20;
+        int expectedResult = 5;
+        long result = calculator.subtract(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
 }
